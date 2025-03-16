295f5ee0df58508980c18935158562e78f9f5fc6
rahulchy960 <rahulchy960@gmail.com>
2025-03-16 19:00:01 +0530

    Letter Combination of phone keypad

--- Back Tracking/02_Combination.cpp
+++ Back Tracking/02_Combination.cpp
@@ -1,17 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<vector<int>>res;
 
+vector<vector<int>>res; // Stores all possible combinations
 
+// Recursive helper function to generate combinations
 void helper(int n, int k, vector<int>&arr, vector<int>&temp, int i=0){
-    if(temp.size()==k){
+
+    if(temp.size()==k){   // Base case: If the combination reaches the required size (k), add it to results
         res.push_back(temp);
         return;
     }
-    for(int j=i;j<n;j++){
-        temp.push_back(arr[j]);
-        helper(n, k, arr, temp, j+1);
-        temp.pop_back();
+    for(int j=i;j<n;j++){   // Iterate through remaining elements starting from index i
+        temp.push_back(arr[j]);  // Add the current element to the combination
+        helper(n, k, arr, temp, j+1);  // Recursive call for the next index
+        temp.pop_back();   // Backtrack by removing the last element
     }
 }
 
--- Back Tracking/07_Letter_Combination_Of_A_Phone_Number.cpp
+++ Back Tracking/07_Letter_Combination_Of_A_Phone_Number.cpp
@@ -3,17 +3,30 @@ using namespace std;
 vector<string>res;
 vector<string>phone = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
-void phoneKeypad(string digits, string curr=""){
+void phoneKeypad(string digits, string curr="", int i=0){
+    // Base case: If the current combination length matches the input digits length
     if(curr.length()==digits.length()){
         res.push_back(curr);
         return;
     }
-    for(int i=0;i<digits.length();i++){
-        
+     // Get the current digit's corresponding string and length
+    int len = phone[digits[i]-'0'].length();
+    string letter = phone[digits[i]-'0'];
+
+    // Loop through all possible characters for the current digit
+    for(int j=0;j<len;j++){
+        curr.push_back(letter[j]);  // Add the character to the current combination
+        phoneKeypad(digits,curr,i+1);   // Recursive call for the next digit
+        curr.pop_back();    // Backtrack to try other options
     }
+
+    
 }
 
 int main(){
-    string digits = "23";
+    string digits = "2";
     phoneKeypad(digits);
+    for(auto x : res){
+        cout<<x<<" ";
+    }
 }
