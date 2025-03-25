19361577fbac2f9f067e847faac9750e81552363
rahulchy960 <rahulchy960@gmail.com>
2025-03-25 05:51:49 +0530

    dynamic programming

--- Dynamic Programming/03_LCS.cpp
+++ Dynamic Programming/03_LCS.cpp
@@ -1,12 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*  Recursive solution
+
+int LCS(string s1, string s2, int m, int n){
+    if(n==0 || m==0) return 0;
+    if(s1[m-1] == s2[n-1]) 
+        return 1 + LCS(s1,s2,m-1,n-1);
+    return max(LCS(s1,s2,m-1,n), LCS(s1,s2,m,n-1));
+}
+
+*/
+
+/*  Memorization based solution
+
+int LCS(string &s1, string &s2,int m, int n, vector<vector<int>>&memo){
+    if(memo[m][n]==-1){
+        if(m==0 || n==0)
+            return 0;
+        else if(s1[m-1] == s2[n-1])
+            memo[m][n] = 1 + LCS(s1,s2,m-1,n-1,memo);
+        else
+            memo[m][n] = max(LCS(s1,s2,m-1,n,memo), LCS(s1,s2,m,n-1,memo));
+    }
+    return memo[m][n]; 
+}
+
+*/
+
+
+// LCS tabulation based solution
+int LCS(string s1, string s2){
+    int m = s1.length();
+    int n = s2.length();
+    int dp[m+1][n+1];
+    for(int i=0;i<=m;i++)dp[i][0] = 0;
+    for(int j=0;j<=n;j++)dp[0][j] = 0;
+
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s1[i-1] == s2[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
+
+
 
 
 int main(){
-    string s1 = "XYZ";
-    string s2 = "AXBZ";
+    string s1 = "abc";
+    string s2 = "def";
     int m = s1.length();
     int n = s2.length();
-     
+
+    // cout<<LCS(s1,s2,m,n)<<endl;  
+
+    // vector<vector<int>>memo(m+1,vector<int>(n+1,-1));
+    // cout<<LCS(s1,s2,m,n,memo)<<endl;  
+
+    cout<<LCS(s1,s2)<<endl;
+
+
 }
---
+++ Dynamic Programming/05_Coin_Change_2.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+int coinChange(vector<int>&coins, int n, int amount){
+    if(amount == 0)return 1;
+    if(n==0) return 0;
+
+    int res = coinChange(coins, n-1, amount);
+
+    if(coins[n-1]<= amount)
+        res = res + coinChange(coins, n, amount - coins[n-1]); 
+    
+    return res;
+}
+*/
+
+/*
+int coinChange(vector<int>&coins, int n, int amount, vector<vector<int>>&memo){
+    if(amount == 0) memo[amount][n] = 1;
+    if(n==0) memo[amount][n] = 0;
+    if(memo[amount][n] == -1){
+        int case1 = coinChange(coins,n-1,amount,memo);
+
+        int case2=0;
+        if(coins[n-1]<=amount)
+            case2 = coinChange(coins,n,amount-coins[n-1],memo);
+        
+        memo[amount][n] = case1 + case2;
+    }
+    return memo[amount][n];
+}
+*/
+
+int coinChange(vector<int>&coins, int n, int amount){
+    int dp[amount+1][n+1];
+    for(int i=0;i<=n;i++)dp[0][i] = 1;
+    for(int i=1;i<=amount;i++) dp[i][0] = 0;
+
+    for(int i=1;i<=amount;i++){
+        for(int j=1;j<=n;j++){
+            int notTaken = dp[i][j-1];
+            int taken  = 0;
+            if(coins[j-1]<=i) 
+                taken = dp[i-coins[j-1]][j];
+            dp[i][j] = taken + notTaken;
+            
+        }
+    }
+    return dp[amount][n];
+}
+
+int main(){
+    
+    vector<int>coins = {1,2,5};
+    int amount = 5;
+    // cout<<coinChange(coins,coins.size(),amount)<<endl;
+
+    int n = coins.size();
+    // vector<vector<int>>memo(amount+1,vector<int>(n+1, -1));
+    // cout<<coinChange(coins,n,amount,memo)<<endl;
+
+    cout<<coinChange(coins,n, amount)<<endl;
+}
