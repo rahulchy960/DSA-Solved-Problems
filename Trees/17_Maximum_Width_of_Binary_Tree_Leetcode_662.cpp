7d97e85d6eb5621323d2b71af650077e8dc69e6f
rahulchy960 <rahulchy960@gmail.com>
2025-06-08 04:30:56 +0530

    Max Width

--- Trees/17_Maximum_Width_of_Binary_Tree_Leetcode_662.cpp
+++ Trees/17_Maximum_Width_of_Binary_Tree_Leetcode_662.cpp
@@ -1,45 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        if (n == 0 || k == 0) return 0;
-
-        // If k >= n/2, we can perform unlimited transactions
-        if (k >= n / 2) {
-            int maxProfit = 0;
-            for (int i = 1; i < n; i++) {
-                maxProfit += abs(prices[i] - prices[i - 1]);
-            }
-            return maxProfit;
-        }
-
-        // dp[t][d] = max profit at day d with at most t transactions
-        vector<vector<int>> dp(k + 1, vector<int>(n, 0));
-
-        for (int t = 1; t <= k; ++t) {
-            // These track the max values for normal and short transactions
-            int maxNormal = -prices[0];
-            int maxShort = prices[0];
-            for (int d = 1; d < n; ++d) {
-                // Max of:
-                // - No transaction at day d
-                // - Sell today after buying earlier (normal)
-                // - Buy back today after short-selling earlier
-                dp[t][d] = max({
-                    dp[t][d - 1],                         // No trade
-                    prices[d] + maxNormal,               // Normal buy-sell
-                    maxShort - prices[d]                 // Short-sell then buy back
-                });
-
-                // Update maxNormal and maxShort
-                maxNormal = max(maxNormal, dp[t - 1][d - 1] - prices[d]);
-                maxShort = max(maxShort, dp[t - 1][d - 1] + prices[d]);
+    int widthOfBinaryTree(TreeNode* root) {
+        queue<pair<TreeNode*, int>>dq;
+        long long res = 1;
+        dq.push({root, 0});
+        while(!dq.empty()){
+            long long end = dq.back().second ;
+            long long start = dq.front().second ;
+            long long width = end - start + 1; 
+            res = max(res, width);
+            int n = dq.size();
+            for(int i=0;i<n;i++){
+                
+                TreeNode* curr = dq.front().first;
+                int idx = dq.front().second;
+                
+                dq.pop();
+                
+                long long indexLeft = 2*(long long)idx + 1;
+                long long indexRight = 2*(long long)idx + 2;
+                
+                if(curr->left != NULL) dq.push({curr->left, indexLeft});
+                if(curr->right != NULL) dq.push({curr->right, indexRight});
             }
         }
-
-        return dp[k][n - 1];
+        return res;
     }
 };
